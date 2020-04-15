d25597441db295546a7b3757ad497125604daf6b
jpzk <jendrik@madewithtea.com>
2020-04-15 16:07:34 +0200

    added more data

--- static_data/download-latest.sh
+++ static_data/download-latest.sh
@@ -4,3 +4,4 @@ curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=ETH > api/latest-
 curl https://www.tokenanalyst.io/api/latest-exchange-stats?tokens=BTC > api/latest-exchange-stats_tokens_BTC
 curl https://www.tokenanalyst.io/api/latest-exchange-stats?tokens=ETH > api/latest-exchange-stats_tokens_ETH
 curl "https://www.tokenanalyst.io/api/exchange-flows?token=BTC&exchange=Binance&timeWindow=1d" > api/exchange-flows_token_BTC_exchange_Binance_timeWindow_1d
+curl "https://www.tokenanalyst.io/api/data-api-config" > data-api-config
