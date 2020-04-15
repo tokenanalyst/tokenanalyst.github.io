f18561f6b3afd2958f1c5154ce71f4b9fbeae1c5
jpzk <jendrik@madewithtea.com>
2020-04-15 15:04:46 +0200

    added static files

---
+++ download-latest.sh
@@ -0,0 +1,4 @@
+curl https://www.tokenanalyst.io/api/exchange-io > exchange-io
+curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > latest-miner-stats?tokens=BTC
+curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > latest-miner-stats?tokens=ETH
+
