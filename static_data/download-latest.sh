0b5d4a85dfdfef6599190da5cccb6ff845d9c6eb
jpzk <jendrik@madewithtea.com>
2020-04-15 15:26:26 +0200

    changed name of dataset

--- static_data/download-latest.sh
+++ static_data/download-latest.sh
@@ -1,4 +1,4 @@
 curl https://www.tokenanalyst.io/api/exchange-io > api/exchange-io
-curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > api/latest-miner-stats?tokens=BTC
-curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > api/latest-miner-stats?tokens=ETH
+curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > api/latest-miner-stats_tokens_BTC
+curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > api/latest-miner-stats_tokens_ETH
 
