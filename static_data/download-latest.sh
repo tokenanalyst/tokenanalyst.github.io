996756720c8f485a194b56079caff0464476fb50
jpzk <jendrik@madewithtea.com>
2020-04-15 15:55:41 +0200

    added dataset

--- static_data/download-latest.sh
+++ static_data/download-latest.sh
@@ -1,4 +1,6 @@
 curl https://www.tokenanalyst.io/api/exchange-io > api/exchange-io
 curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > api/latest-miner-stats_tokens_BTC
-curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=BTC > api/latest-miner-stats_tokens_ETH
+curl https://www.tokenanalyst.io/api/latest-miner-stats?tokens=ETH > api/latest-miner-stats_tokens_ETH
+curl https://www.tokenanalyst.io/api/latest-exchange-stats?tokens=BTC > api/latest-exchange-stats_tokens_BTC
+curl https://www.tokenanalyst.io/api/latest-exchange-stats?tokens=ETH > api/latest-exchange-stats_tokens_ETH
 
