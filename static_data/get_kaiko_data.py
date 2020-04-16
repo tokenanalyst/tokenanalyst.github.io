5e3ba3d073be8e3be2110c6732d820b778543b52
sidshekhar <sidshekhar27@gmail.com>
2020-04-16 14:50:00 +0100

    added btc binance with ampersand

--- static_data/get_kaiko_data.py
+++ static_data/get_kaiko_data.py
@@ -10,4 +10,4 @@ months = [{"start_date": "2020-01-01", "end_date": "2020-01-31"},
 for month in months:
     url_params = f"instrument={token_pair}&instrument_class=spot&interval=1d&start_time={month['start_date']}T00:00:00.000Z&end_time={month['end_date']}T23:59:59.999Z&exchange={exchange}&commodity=trades&page_size=1000"
     urllib.request.urlretrieve(f"https://www.tokenanalyst.io/api/kaiko-historical-aggregates?{url_params}",
-                               f"api/kaiko-historical-aggregates_{url_params}")
+                               f"api/kaiko-historical-aggregates&{url_params}")
